db892af33adae41f4bca74e121a88de90713cf52
Quang Truong <qtruongngoc95@gmail.com>
2023-12-17 17:35:32 +0200

    fix: refactor lofi videos code

--- src/components/Projects/Lofi/UI/Home/LofiHome.tsx
+++ src/components/Projects/Lofi/UI/Home/LofiHome.tsx
@@ -1,6 +1,7 @@
 import { useLofiStore } from "../../../../../store/lofiStore";
 import { LOFI_BACKGROUND, LOFI_WEATHER } from "../../../../../types";
 
+
 const LofiHome = () => {
   const [currentDayMode, currentWeatherMode] = useLofiStore((state) => [
     state.currentDayMode,
@@ -11,57 +12,24 @@ const LofiHome = () => {
   return (
     <div className="relative flex align-middle items-center justify-between">
       <div className="absolute lofi-video">
-        <video
-          className={
-            combineMode === `${LOFI_BACKGROUND.DAY}-${LOFI_WEATHER.SUNNY}`
-              ? "videoIn"
-              : "videoOut"
-          }
-          autoPlay
-          loop
-          muted
-        >
-          <source src="/assets/background/Day-sunny.mp4" type="video/mp4" />
-        </video>
-
-        <video
-          className={
-            combineMode === `${LOFI_BACKGROUND.DAY}-${LOFI_WEATHER.RAIN}`
-              ? "videoIn"
-              : "videoOut"
-          }
-          autoPlay
-          loop
-          muted
-        >
-          <source src="/assets/background/Day-rainny.mp4" type="video/mp4" />
-        </video>
-
-        <video
-          className={
-            combineMode === `${LOFI_BACKGROUND.NIGHT}-${LOFI_WEATHER.SUNNY}`
-              ? "videoIn"
-              : "videoOut"
-          }
-          autoPlay
-          loop
-          muted
-        >
-          <source src="/assets/background/Night-clear.mp4" type="video/mp4" />
-        </video>
-
-        <video
-          className={
-            combineMode === `${LOFI_BACKGROUND.NIGHT}-${LOFI_WEATHER.RAIN}`
-              ? "videoIn"
-              : "videoOut"
-          }
-          autoPlay
-          loop
-          muted
-        >
-          <source src="/assets/background/Night-rainny.mp4" type="video/mp4" />
-        </video>
+        {[
+          { bg: LOFI_BACKGROUND.DAY, weather: LOFI_WEATHER.SUNNY, videoSrc: "/assets/background/Day-sunny.mp4" },
+          { bg: LOFI_BACKGROUND.DAY, weather: LOFI_WEATHER.RAIN, videoSrc: "/assets/background/Day-rainny.mp4" },
+          { bg: LOFI_BACKGROUND.NIGHT, weather: LOFI_WEATHER.SUNNY, videoSrc: "/assets/background/Night-clear.mp4" },
+          { bg: LOFI_BACKGROUND.NIGHT, weather: LOFI_WEATHER.RAIN, videoSrc: "/assets/background/Night-rainny.mp4" },
+        ].map((config, index) => (
+          <video
+            key={index}
+            className={`${
+              combineMode === `${config.bg}-${config.weather}` ? "videoIn" : "videoOut"
+            }`}
+            autoPlay
+            loop
+            muted
+          >
+            <source src={config.videoSrc} type="video/mp4" />
+          </video>
+        ))}
       </div>
     </div>
   );
