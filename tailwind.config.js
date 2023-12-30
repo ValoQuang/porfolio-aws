99d507551a5a4ee6bb53aa2e8d30c4cbe5fbb918
Quang Truong <qtruongngoc95@gmail.com>
2023-12-30 02:21:28 +0200

    fix: css

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       padding: {
         navbar: "0px 48px 0px 48px;",
-        main: "60px 80px 0px 80px;",
+        main: "60px 80px 70% 80px;",
         link: "4.59px 3.204px 5.58px 3.6px",
         footer: "10px 72px 100px 72px;",
       },
