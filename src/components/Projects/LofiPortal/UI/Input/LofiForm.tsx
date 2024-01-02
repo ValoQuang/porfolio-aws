39339f590f21b39da99021cd51ff3e8264549f75
Q.ilot <45687913+ValoQuang@users.noreply.github.com>
2024-01-02 22:10:27 +0200

    fix: naming practice

--- src/components/Projects/LofiPortal/UI/Input/LofiForm.tsx
+++ src/components/Projects/LofiPortal/UI/Input/LofiForm.tsx
@@ -12,16 +12,18 @@ export interface InputObjectProp {
   username?: string;
 }
 
+const inputState = {
+  username: "",
+  email: "",
+  password: "",
+}
+
 const LofiForm: React.FC = () => {
   const [isSignUp, setIsSignUp] = useState(false);
-  const [inputObject, setInputObject] = useState<InputObjectProp>({
-    username: "",
-    email: "",
-    password: "",
-  });
+  const [inputObject, setInputObject] = useState<InputObjectProp>(inputState);
   const [error, setError] = useState<string | null>(null);
 
-  const submitHandler = async (event: { preventDefault: () => void }) => {
+  const submitFormHandler = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
     try {
       const response = await UseFetch(
@@ -34,6 +36,7 @@ const LofiForm: React.FC = () => {
       );
       if (response.error) {
         setError(response.error);
+        setInputObject(inputState);
       }
       setInLocalStorage(LOCAL_STORAGE.USER, JSON.stringify(response));
     } catch (error) {
@@ -41,7 +44,7 @@ const LofiForm: React.FC = () => {
     }
   };
 
-  const signUpHandler = () => {
+  const toggleSignUp = () => {
     setIsSignUp(!isSignUp);
   };
 
@@ -62,19 +65,16 @@ const LofiForm: React.FC = () => {
         {isSignUp ? (
           <>
             <LofiSignUp inputObject={inputObject} setInputObject={setInputObject}/>
-
-            {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
           </>
         ) : (
           <>
             <LofiSignIn inputObject={inputObject} setInputObject={setInputObject} />
-            {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
           </>
         )}
 
         <div className="pt-10">
           <button
-            onClick={submitHandler}
+            onClick={submitFormHandler}
             type="submit"
             className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm transition duration-100 hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
           >
@@ -84,13 +84,14 @@ const LofiForm: React.FC = () => {
           <div className="flex gap-5 items-center mt-10 text-center text-sm text-gray-500">
             {isSignUp ? "Already have an account ?" : "Not a member ?"}
             <div
-              onClick={signUpHandler}
+              onClick={toggleSignUp}
               className="hover:cursor-pointer font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               {isSignUp ? "Sign in" : "Sign up"}
             </div>
           </div>
         </div>
+        {error && !isSignUp && <div className="text-red-500 text-sm mt-2">{error}</div>}
       </div>
     </div>
   );
